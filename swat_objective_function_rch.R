07855146ff56769b917fe4e18450210f53fc2ab9
vtdrfuka <80080115+vtdrfuka@users.noreply.github.com>
2022-02-09 08:56:09 -0700

    Moving SWATtoSQLite

--- swat_objective_function_rch.R
+++ swat_objective_function_rch.R
@@ -42,15 +42,3 @@ swat_objective_function_rch<-function (x, calib_range, calib_params, flowgage, r
   file.remove(tmpdir)
   return(abs(NS - 1))
 }
-
-swatsqlite=function(){
- output_hru=readSWAT("hru",".")
- output_sub=readSWAT("sub",".")
- output_rch=readSWAT("rch",".")
- sqlitefile=paste0("./",args$swatscen,"MINTSWATtables.sqlite")
- con <- dbConnect(RSQLite::SQLite(), sqlitefile)
- dbWriteTable(con, "output_hru", output_hru,overwrite = TRUE)
- dbWriteTable(con, "output_rch", output_rch,overwrite = TRUE)
- dbWriteTable(con, "output_sub", output_sub,overwrite = TRUE)
- dbListTables(con)
-}
