62a9b98a2f2d492834c5c5c6fb5d960f0be20f65
vtdrfuka <80080115+vtdrfuka@users.noreply.github.com>
2022-03-28 22:28:04 -0600

    Update SWATMINT0.2.R

--- SWATMINT0.2.R
+++ SWATMINT0.2.R
@@ -1,7 +1,8 @@
 # If not using swat docker container:
 # system("svn checkout svn://scm.r-forge.r-project.org/svnroot/ecohydrology/"); install.packages(c("ecohydrology/pkg/EcoHydRology/","ecohydrology/pkg/SWATmodel/"),repos = NULL)
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(SWATmodel,RSQLite,argparse,stringi,stringr,rgdal,ggplot2,rgeos,rnoaa,moments,sf,readr,tools,diffobj,png,grid,gridExtra,ncdfgeom)
+pacman::p_load(SWATmodel,RSQLite,argparse,stringi,stringr,rgdal,ggplot2,rgeos,rnoaa,moments,sf,readr,tools,
+               diffobj,png,grid,gridExtra,ncdfgeom,purrr,raster,sampSurf)
 source("https://raw.githubusercontent.com/mintproject/MINTSWATmodel/main/MINTSWATcalib.R")
 source("https://raw.githubusercontent.com/mintproject/MINTSWATmodel/main/SWAToutput.R")
 source("https://r-forge.r-project.org/scm/viewvc.php/*checkout*/pkg/EcoHydRology/R/get_grdc_gage.R?root=ecohydrology")
